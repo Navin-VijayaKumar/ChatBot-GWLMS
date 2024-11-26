fa3b8b2fb7c1fca7cebf1ec56346dfa843a28e5f
Navin-VijayaKumar <navinv10122004@gmail.com>
2024-11-26 20:26:24 +0530

    commit one

---
+++ ChatBot.py
@@ -0,0 +1,5 @@
+from langchain_core.prompts import ChatPromptTemplate
+#ChatPromptTemplate use to create templates 'hi-hi how are you'
+from langchain_core.output_parsers import StrOutputParser
+#StrOutputParser removes special char like \n for next line ,make easy to understand
+
