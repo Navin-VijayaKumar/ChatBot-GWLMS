3e8a9592b3d41062d6a91a158bd97554f6d6db93
Navin-VijayaKumar <navinv10122004@gmail.com>
2024-12-01 19:55:11 +0530

    commit two

--- ChatBot.py
+++ ChatBot.py
@@ -1,5 +1,23 @@
-from langchain_core.prompts import ChatPromptTemplate
-#ChatPromptTemplate use to create templates 'hi-hi how are you'
-from langchain_core.output_parsers import StrOutputParser
-#StrOutputParser removes special char like \n for next line ,make easy to understand
+from langchain_community.llms.ollama import Ollama
 
+from langchain_community.llms.ollama import Ollama
+from langchain_community.llms.ollama import Ollama
+import streamlit as st
+
+st.title("Jarvis")
+input_txt = st.text_input("Please enter the queries here.")
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "you are a helpful AI assistant. Your name is Jarvis"),
+        ("user", "user query:{query}")
+    ]
+)
+
+llm = Ollama(model="llama2")  # Use the correct callable object from the ollama module
+output_parser = StrOutputParser()
+
+chain = prompt|llm | output_parser
+
+if input_txt:
+    st.write(chain.invoke({"query": input_txt}))
